667777236432095fcf703b4ed9f4d5da52c4acc1
Tiarnach O Riada <tiarnachreidy@gmail.com>
2020-11-01 19:27:08 +0000

    P5 Done

--- CT0_5.m
+++ CT0_5.m
@@ -4,32 +4,60 @@ b=6;
 c=6;
 
 set(gca, 'defaultTextInterpreter','latex')
+set(gca, 'FontSize',54)
 close all
 %% Problem Five
-% EXAMPLE ODE
-exampleODE = @(x,y) -(2*x) -y;
-% [TOUT,YOUT] = ODE45(ODEFUN,TSPAN,Y0), TSPAN = [T0 TFINAL]
-% integrates y' = f(t,y) from t=T0 to t=TFINAL with initial conditions Y0
-X0 = 0;
-XFINAL = 3;
-Y0 = -1;
-%[X, Y] = ode45(exampleODE, [X0 XFINAL], Y0);
-
 % QUESTION'S ODE
 g = 9.81; % m s^-1
 A = a+b+2*c;
 x_r = a;
 C_d = c/2;
 A_d = a/3;
-fn = @(qin, xt) (C_d*A_d*sqrt(2*g))/A*(sqrt(x_r)-sqrt(x_r-xt)) + qin/A;
+fn = @(qin, t, xt) (C_d*A_d*sqrt(2*g))/A*(sqrt(x_r)-sqrt(x_r+xt)) + qin/A;
+
+Q0 = C_d*A_d * sqrt(2*g*x_r);
 
 T0 = 0;
-T1 = 24;
+T1 = 40;
 XT0 = x_r;
-[T, XT] = ode45(fn, [T0 T1], XT0);
-plot(T,abs(XT))
-hold on
-plot(T,angle(XT))
-hold off
+
+step = [0.001 0.05 0.1:0.2:1];
+
+for i = 1:length(step)
+    q0 = Q0 * (step(i));
+    f = @(t, xdash) fn(q0, t, xdash);
+    [t, y] = ode45(f, [T0 T1], X0);
+    plot(t, y, 'DisplayName', strcat('$\tilde{q}_{in} =$',num2str(step(i)),'$\times Q_{in,r}$'))
+    hold on
+end
+title('$\tilde{x}$ for several $\tilde{q}_{in}$', 'Interpreter', 'latex')
 xlabel('$t$','Interpreter','latex')
-ylabel('$\dot{\tilde{x}}$','Interpreter','latex')
+ylabel('$\tilde{x}$','Interpreter','latex')
+lgd = legend;
+set(lgd, 'Interpreter', 'latex')
+set(gca, 'FontSize',18)
+%print('report/img/p5-qin','-dpng');
+
+%% LINEARISE IT
+close all
+fn_lin = @(qin, t, xt) (qin/A) - (C_d*A_d*sqrt(g))/(sqrt(2*x_r)*A)*xt;
+
+step = [0.001 0.1 0.3 0.5];
+
+for i = 1:length(step)
+    q0 = Q0 * (step(i));
+    f = @(t, xdash) fn(q0, t, xdash);
+    [t, y] = ode45(f, [T0 T1], X0);
+    fl = @(t, xdash) fn_lin(q0, t, xdash);
+    [tl, yl] = ode45(fl, [T0 T1], X0);
+    plot(t, y, 'x', 'DisplayName', strcat('Global model: $\tilde{q}_{in} =$',num2str(step(i)),'$\times Q_{in,r}$'))
+    hold on
+    plot(tl, yl, 'DisplayName', strcat('Local model: $\tilde{q}_{in} =$',num2str(step(i)),'$\times Q_{in,r}$'))
+end
+title('Local linearised vs global model of $\tilde{x}$ for several $\tilde{q}_{in}$', 'Interpreter', 'latex')
+xlabel('$t$','Interpreter','latex')
+ylabel('$\tilde{x}$','Interpreter','latex')
+lgd = legend;
+set(lgd, 'Interpreter', 'latex')
+set(gca, 'FontSize',18)
+print('report/img/p5-lin','-dpng');
